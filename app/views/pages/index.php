cb29ca1422024dd7b575ddfeaac012f7f40583c0
zStevenn <steventje678@hotmail.com>
2022-03-18 08:38:06 +0100

    Created link to countries index page

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,9 +1,13 @@
 <?php
 // echo $data['name'];
 // echo URLROOT;
-var_dump($data);
+// var_dump($data);
 
-foreach ($data['users'] as $user) {
-  echo "Information: " . $user->user_name . $user->user_email;
-  echo "<br>";
-}
+// foreach ($data['users'] as $user) {
+//   echo "Information: " . $user->user_name . $user->user_email;
+//   echo "<br>";
+// }
+?>
+<h1>
+  <a href="<?= URLROOT ?>/countries/index">Landenoverzicht</a>
+</h1>
